a368fc17ec89fedfbc26586254af0237a5ea86a1
yinghdb <yinghdb@163.com>
2016-06-03 22:22:57 +0800

    create new tables

--- database/models.py
+++ database/models.py
@@ -5,28 +5,31 @@ from django.db import models
 
 # Create your models here.
 
+# 用户表
 class UserTable(models.Model):
-	UserID = models.CharField()
-	Name = models.CharField()
-	IDcard = models.CharField()
+	UserID = models.CharField(max_length=20)
+	Name = models.CharField(max_length=20)
+	IDcard = models.CharField(max_length=20)
 	Gender = models.IntegerField()
-	Occupation = models.CharField()
-	EduInfo = models.CharField()
+	Occupation = models.CharField(max_length=20)
+	EduInfo = models.CharField(max_length=20)
 	HomeAddr = models.TextField()
 	Department = models.TextField()
-	Tel = models.CharField()
-	MailAddr = models.CharField()
+	Tel = models.CharField(max_length=20)
+	MailAddr = models.CharField(max_length=20)
 	Age = models.IntegerField()
 
+# 证券账户基本信息
 class SecurityAccountInfo(models.Model):
-	SecurityID = models.CharField()
+	SecurityID = models.CharField(max_length=20)
 
+# 资金账户基本信息
 class CapitalAccountInfo(models.Model):
-	AccountID = models.CharField()
-	Password = models.CharField()
+	AccountID = models.CharField(max_length=20)
+	Password = models.CharField(max_length=20)
 	Isfirst = models.BooleanField(default=1)
 	UserTable = models.ForeignKey(UserTable)
-	BuyPassword = models.CharField()
+	BuyPassword = models.CharField(max_length=20)
 	SecurityAccount = models.ForeignKey(SecurityAccountInfo)
 	loginPwdWrongNum = models.IntegerField(default=0)
 	transPwdWrongNum = models.IntegerField(default=0)
@@ -35,25 +38,12 @@ class CapitalAccountInfo(models.Model):
 	IsTransFreeze = models.BooleanField()
 	IsLoginFreeze = models.BooleanField()
 
-class UserTable(models.Model):
-	UserID = models.CharField()
-	Name = models.CharField()
-	IDcard = models.CharField()
-	Gender = models.IntegerField()
-	Occupation = models.CharField()
-	EduInfo = models.CharField()
-	HomeAddr = models.TextField()
-	Department = models.TextField()
-	Tel = models.CharField()
-	MailAddr = models.CharField()
-	Age = models.IntegerField()
-
 class InstDealed(models.Model): #the table of Dealed Instructions
-	InstID = models.CharField()
+	InstID = models.CharField(max_length=20)
 	TimeSubmit = models.DateTimeField()
 	TimeDealed = models.DateTimeField()
 	InstType = models.IntegerField() # buy/sell
-	StockID = models.CharField()
+	StockID = models.CharField(max_length=20)
 	AccountID = models.ForeignKey(CapitalAccountInfo)
 	SecurityID = models.ForeignKey(SecurityAccountInfo)
 	Quantity = models.IntegerField()
@@ -61,12 +51,27 @@ class InstDealed(models.Model): #the table of Dealed Instructions
 	PriceDealed = models.FloatField() #dealed price
 
 class InstOutOfDate(models.Model): #the table of Dealed Instructions
-	InstID = models.CharField()
+	InstID = models.CharField(max_length=20)
 	TimeSubmit = models.DateTimeField()#submit time
 	TimeOutOfDate = models.DateTimeField() #out of date time
 	InstType = models.IntegerField() # buy/sell
-	StockID = models.CharField() 
+	StockID = models.CharField(max_length=20) 
 	AccountID = models.ForeignKey(CapitalAccountInfo)
 	SecurityID = models.ForeignKey(SecurityAccountInfo)
 	Quantity = models.IntegerField()
 	PriceSubmit = models.FloatField() #submit price
+
+# 资金信息
+class CapitalInfo(models.Model):
+	AccountID = models.CharField(max_length=20)
+	ActiveMoney = models.FloatField()
+	FrozenMoney = models.FloatField()
+	BankCard = models.CharField(max_length=20)
+
+# 证券账户持有股票信息
+class SecurityStockInfo(models.Model):
+	SecurityID = models.CharField(max_length=20)
+	StockID = models.CharField(max_length=20)
+	ShareHolding = models.IntegerField()	#股数
+	status = models.IntegerField()
+	BuyPrice = models.FloatField()
