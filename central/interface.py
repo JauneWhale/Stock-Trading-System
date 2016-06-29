87dbf21931aecf53a11631a9f6b965c008065bdf
yinghdb <yinghdb@163.com>
2016-06-29 13:13:47 +0800

    change central

--- central/interface.py
+++ central/interface.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python2.7 
-import sys;
-import os;
+import sys
+import os
 
 # NOTE: PLEASE comment or edit this line if you want to test the module yourself
-# sys.path.append('C:/users/shorc/documents/github/stock_trading_system/');
-#sys.path.append('/home/jj/gitProject/sst2/Stock_Trading_System');
+# sys.path.append('C:/users/shorc/documents/github/stock_trading_system/')
+#sys.path.append('/home/jj/gitProject/sst2/Stock_Trading_System')
 
 # environment stuff
-from django.apps import apps;
-from django.conf import settings;
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Stock_Trading_System.settings");
-from django.core.management import execute_from_command_line;
-#execute_from_command_line(sys.argv);
+from django.apps import apps
+from django.conf import settings
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Stock_Trading_System.settings")
+from django.core.management import execute_from_command_line
+#execute_from_command_line(sys.argv)
 # ~~env
 
 # database dependence
-from database.models import *;
+from database.models import *
 
 # in-module dependence
-import globalvar;
-from priorityQueue import PriorityQueue;
+import globalvar
+from priorityQueue import PriorityQueue
 
 glob = globalvar.GlobalVar()
 instIdCount = 0
@@ -27,94 +27,94 @@ instIdCount = 0
 def init_central_trading_system():
 	global glob
 
-	stockInfoList = StockInfo.objects.all();
-	securityStockInfoList = SecurityStockInfo.objects.all();	# this is accounts!!!
+	stockInfoList = StockInfo.objects.all()
+	securityStockInfoList = SecurityStockInfo.objects.all()	# this is accounts!!!
 	
 	for iStock in stockInfoList:
-		glob.InstQueue[iStock.StockID] = (PriorityQueue(),PriorityQueue());
+		glob.InstQueue[iStock.StockID] = (PriorityQueue(),PriorityQueue())
 	
 	for iAccount in securityStockInfoList:	# iSecStock
-		glob.InstNotDealt[iAccount.SecurityID] = {};
+		glob.InstNotDealt[iAccount.SecurityID] = {}
 
-	return True;
+	return True
 
 def buy(inst):
 	global glob
 	global instIdCount
-	# inst[0] = glob.instIdCount;
-	inst_new = (str(instIdCount),inst[1],inst[2],inst[3],inst[4],inst[5],inst[6],inst[7]);
-	instIdCount += 1;
-	instTp = inst[2];	# instType
-	assert (instTp == 0);
+	# inst[0] = glob.instIdCount
+	inst_new = (str(instIdCount),inst[1],inst[2],inst[3],inst[4],inst[5],inst[6],inst[7])
+	instIdCount += 1
+	instTp = inst[2]	# instType
+	assert (instTp == 0)
 
 	# print inst_new
 	
-	glob.match(inst_new);
+	glob.match(inst_new)
 
 def sell(inst):
 	global glob
 	global instIdCount
-	# inst[0] = glob.instIdCount;
-	inst_new = (str(instIdCount),inst[1],inst[2],inst[3],inst[4],inst[5],inst[6],inst[7]);
-	instIdCount += 1;
-	instTp = inst[2];
-	assert (instTp == 1);
+	# inst[0] = glob.instIdCount
+	inst_new = (str(instIdCount),inst[1],inst[2],inst[3],inst[4],inst[5],inst[6],inst[7])
+	instIdCount += 1
+	instTp = inst[2]
+	assert (instTp == 1)
 
 	# print inst_new
 
-	glob.match(inst_new);
+	glob.match(inst_new)
 
 def query(securityId):
 	global glob
 	print glob.InstNotDealt
 	if glob.InstNotDealt.has_key(securityId):
-		return glob.InstNotDealt[securityId];
+		return glob.InstNotDealt[securityId]
 	else:
 		return {}
 
-def admin_query():
-	global glob
-	Stocklist = {};
-	buy_list=[];
-	sell_list=[];
-	for account in glob.InstNotDealt:
-		for inst in glob.InstNotDealt[account]:
-			_inst = glob.InstNotDealt[account][inst];
-			if(_inst[2] == 0): #buy
-				buy_list.append({"id":_inst[0],"p_id":_inst[4],"price":_inst[7],"amount":_inst[6]});	
-			elif(_inst[2]==1): #sell
-				sell_list.append({"id":_inst[0],"s_id":_inst[4],"price":_inst[7],"amount":_inst[6]});
-	Stocklist={"buy_list":buy_list,"sell_list":sell_list};
-	return Stocklist;
+def admin_query(stockId):
+    global glob
+    Stocklist = {}
+    buy_list=[]
+    sell_list=[]
+
+    buy_queue = glob.InstQueue[stockId][0]
+    sell_queue = glob.InstQueue[stockId][1]
+    for i in range (0,buy_queue.qsize()):
+        buy_list.append(buy_queue.queue[i][1])
+    for j in range (0,sell_queue.qsize()):
+        sell_list.append(sell_queue.queue[j][1])
+    Stocklist={"buy_list":buy_list,"sell_list":sell_list}
+    return Stocklist
 
 def revoke(instId, s_id):
 	global glob
-	glob.delete(instId, s_id);
+	glob.delete(instId, s_id)
 
 def renew(stockId):
-	stockInfo = StockInfo.objects.get(StockID=stockId);
-	stockInfo.state=0;
+	stockInfo = StockInfo.objects.get(StockID=stockId)
+	stockInfo.state=0
 	
 def froze(stockId):
 	global glob
-	stockInfo = StockInfo.objects.get(StockID=stockId);
-	stockInfo.state=1;
-	#glob.flush(stockId); #clear the InstNotDealt map and the queue, push them into the database
+	stockInfo = StockInfo.objects.get(StockID=stockId)
+	stockInfo.state=1
+	#glob.flush(stockId) #clear the InstNotDealt map and the queue, push them into the database
 
-# init_central_trading_system()
+init_central_trading_system()
 
 #inst = (ID,time,type,StockID,securityID,accountID,quantity,price) 
 #		[0] [1]  [2]  [3]		[4]			 [5]	   [6]	  [7]
 
-# inst=('0000002','201606071945',0,'0001','3130102347','123',400,124.32);
-# inst2=('0000004','201606071945',0,'0001','3130102346','123',400,124.32);
-# inst3=('0000005','201606071945',1,'0001','3130102234','123',400,124.32);
-# sellI=('0000003','201606071946',1,'0001','3130102345','124',100,124.32);
-# #glob.InstQueue['0001'] = (PriorityQueue(),PriorityQueue());
-# #glob.insert(inst);
-# #glob.insert(inst2);
-# #glob.insert(inst3);
-# # sell(sellI); ! CAN'T DO this because the tables are empty
-# print query('3130102347');
-# print admin_query();
+# inst=('0000002','201606071945',0,'0001','3130102347','123',400,124.32)
+# inst2=('0000004','201606071945',0,'0001','3130102346','123',400,124.32)
+# inst3=('0000005','201606071945',1,'0001','3130102234','123',400,124.32)
+# sellI=('0000003','201606071946',1,'0001','3130102345','124',100,124.32)
+# #glob.InstQueue['0001'] = (PriorityQueue(),PriorityQueue())
+# #glob.insert(inst)
+# #glob.insert(inst2)
+# #glob.insert(inst3)
+# # sell(sellI) ! CAN'T DO this because the tables are empty
+# print query('3130102347')
+# print admin_query()
 
